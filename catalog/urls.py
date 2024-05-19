e6a258175029d39c95705424804ae8b57e57a5b8
konys <Konyshev_PV@bk.ru>
2024-05-19 23:14:36 +0300

    fist

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .apps import CatalogConfig
+from .views import home,contacts
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
+]
