0c2c83b151f71ed2a481579ee7adb377cd8a2c9f
konys <Konyshev_PV@bk.ru>
2024-05-31 23:50:29 +0300

    Создание моделей, использование black linter, применение первой миграции

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+# Создание класса Категория
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Название категории",
+    )
+    description = models.TextField(verbose_name="Описание категории")
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+
+# Создание класса продукт
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="products/", blank=True, null=True, verbose_name="Фото продукта"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        null=True,
+        blank=True,
+        related_name="Products",
+    )
+    price = models.IntegerField(
+        verbose_name="Цена продукта",
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания",
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата изменения",
+    )
+    manufactured_at = models.DateField(
+        verbose_name="Дата изготовления",
+    )
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f"{self.name} {self.description} {self.price}"
+
+    class Meta:
+        verbose_name = "Продукт"  # Настройка для наименования одного объекта
+        verbose_name_plural = "Продукты"  # Настройка для наименования набора объектов
+        ordering = ["name", "price"]
+
+
+
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .apps import CatalogConfig
-from .views import home,contacts
+from .views import home, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path("", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
 ]
